5669ef011a0687467fcb30a7947eb278b35548c6
juanker73 <114301531+juanker73@users.noreply.github.com>
2023-05-03 21:41:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const gallina = document.getElementById("gallina");
+gallina.addEventListener("click", () => {
+  const audio = new Audio("audio/gallina.mp3");
+  audio.play();
+});
+
+const gallo = document.getElementById("gallo");
+gallo.addEventListener("click", () => {
+  const audio = new Audio("audio/gallo.mp3");
+  audio.play();
+});
+
+const pollito = document.getElementById("pollito");
+pollito.addEventListener("click", () => {
+  const audio = new Audio("audio/pollito.mp3");
+  audio.play();
+});
+
+const perro = document.getElementById("perro");
+perro.addEventListener("click", () => {
+  const audio = new Audio("audio/perro.mp3");
+  audio.play();
+});
+
+const gato = document.getElementById("gato");
+gato.addEventListener("click", () => {
+  const audio = new Audio("audio/gato.mp3");
+  audio.play();
+});
+const raton = document.getElementById("raton");
+raton.addEventListener("click", () => {
+  const audio = new Audio("audio/raton.mp3");
+  audio.play();
+});
+const oveja = document.getElementById("oveja");
+oveja.addEventListener("click", () => {
+  const audio = new Audio("audio/oveja.mp3");
+  audio.play();
+});
+const vaca = document.getElementById("vaca");
+vaca.addEventListener("click", () => {
+  const audio = new Audio("audio/vaca.mp3");
+  audio.play();
+});
+const cerdo = document.getElementById("cerdo");
+cerdo.addEventListener("click", () => {
+  const audio = new Audio("audio/cerdo.mp3");
+  audio.play();
+});
+const abeja = document.getElementById("abeja");
+abeja.addEventListener("click", () => {
+  const audio = new Audio("audio/abeja.mp3");
+  audio.play();
+});
+
+
+      
